73c668ad215142cd8bfb66e8037991eb56e2f778
Lynzie Stephens <LynzieStephens@Lynzies-Air.foothills.net>
2018-01-30 17:26:41 -0500

    well

--- secondmoose.py
+++ secondmoose.py
@@ -1,10 +1,24 @@
+def textline(foo, bar):
+	print(' ', end='')
+	for i in range(len(foo)):
+		print(bar, end='')
+	print()
 
+def textbox(foo):
+	textline(foo, '_')
+	print('<', end='')
+	print(foo, end='')
+	print('>')
+	textline(foo, '_')
 
-user = input("Enter message:")
+bar = input("Enter your message:")
+textbox(bar)
 
-print (user)
-from moose import *
-longstring
+fin = open('moose.py')
+
+for line in fin:
+	print(line, end='')
+
 
 
 
---
+++ test.py
@@ -0,0 +1,6 @@
+user = input("Are you happy now?")
+
+if 'y':
+	from moose import *
+	longstring
+
