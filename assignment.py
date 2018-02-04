d828e8be6e2b5df45e029ebe5d6d2c08614f036f
Lynzie Stephens <LynzieStephens@Lynzies-Air.foothills.net>
2018-02-04 14:35:53 -0500

    like..

--- assignment.py
+++ assignment.py
@@ -28,6 +28,19 @@ while(True):
 			sum1 = sum1 + n
 			n = n - 1
 		print("Calculated sum:", sum1)
+	elif choice == '5':
+		n = int(input("Enter preferred number:"))
+		pro1 = 1
+		while(n > 0):
+			pro1 = pro1 * n
+			n = n - 1
+		print("Calculated product:", pro1)
+	elif choice == '6':
+		for i in range(1, 13):
+			for j in range(1, 13):
+				print(i*j, end='')
+				print(' ' *(10-(len(str(i*j)))), end='')
+			print('\n')
 
 
  
